8f43b8dddcde11b5eacd18c4623afbd4ff49eb09
hivend <shivendrashak12@gmail.com>
2021-12-02 12:31:19 +0545

    modified file

--- lab6.py
+++ lab6.py
@@ -1,8 +1,4 @@
-#6)sum of first ten odd numbers
-num = 20
-sum = 0;
-
-for i in range(1, num+1):
-    if(not (i%2) == 0):
-        sum += i;
-print("\n sum of odd numbers from 1 to", num, "is ",sum)
+#6)sum of first ten positive integers odd numbers
+a = [1,3,5,7,9]
+b = sum(a)
+print(b)
